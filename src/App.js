75bca4ead456f4aecfb359fa2fc65a67de5e7c2b
jelenatomasevic <56553558+jelenatomasevic@users.noreply.github.com>
2019-12-10 14:28:02 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Route } from "react-router-dom";
+import Header from "./app/components/Header/Header";
+import Footer from "./app/components/Footer/Footer";
+import Register from "./app/pages/Register/Register";
+import Login from "./app/pages/Login/Login";
+import AboutPage from "./app/pages/About/AboutPage"
+import "./App.css";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+
+      <Route exact path='/' component={Register} />
+      <Route path='/login' component={Login} />
+      <Route exact path='/about' component={AboutPage} />
+      {/*      <Route path='/posts' component={PostsPage} /> */}
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+
+
+/*
+<Header />
+<Login />
+<Footer /> */
